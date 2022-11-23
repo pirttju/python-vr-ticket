04253f2ab2f1469ee7e4d6ff13f4a33cc26c6a6f
Juhani Pirttilahti <juhani.pirttilahti@proxion.fi>
2022-11-23 13:43:01 +0200

    Improvements!

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # python-vr-ticket
+
 Python kikkare for reading Aztec barcodes on VR's train tickets
 
-Requires bitstring and docbarcodes modules to be installed from pip.
+## Dependencies:
+
+docbarcodes package is a wrapper for zxing which requires Java 8.
+
+## Install:
+
+Install packages:
+
+```
+$ pip install -r requirements.txt
+```
+
+## Usage:
+
+```
+$ python python_vr_ticket -i <image>
+```
+
+python_vr_ticket reads the input image and looks for barcodes. When a barcode is found, structured ticket data is printed out.
---
+++ python_vr_ticket.py
@@ -0,0 +1,42 @@
+import os.path
+import sys
+from docbarcodes.extract import process_document
+from ssbdata import SSBData
+
+"""
+def to_bitstring(string):
+    formatted_string = "".join("{0:08b}".format(x, "b") for x in bytearray(string, "iso-8859-1"))
+    return formatted_string
+"""
+
+
+def extract_barcodes(filename):
+    barcodes_raw, barcodes_combined = process_document(filename)
+    return [b._asdict() for b in barcodes_raw]
+
+
+def process_ticket_image(path_to_file):
+    try:
+        barcodes = extract_barcodes(path_to_file)
+        for code in barcodes:
+            raw = code.get("raw")
+            raw_bytes = bytes(raw, encoding="iso-8859-1")
+            data = SSBData(raw_bytes)
+            print(data.__dict__)
+    except:
+        raise
+
+
+def main():
+    args = sys.argv[1:]
+    if len(args) == 1:
+        try:
+            process_ticket_image(args[1])
+        except:
+            print(f"Error: File {args[1]} does not exist!")
+    else:
+        print("Usage: python_vr_ticket <path_to_image>")
+
+
+if __name__ == "__main__":
+    main()
